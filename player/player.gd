d99a0c73bf3e02caf7ff235d3fe6ada61549f310
Suchaaver Chahal <suchaaverchahal@gmail.com>
2026-07-24 08:45:42 -0400

    improve tiling

--- player/player.gd
+++ player/player.gd
@@ -1,35 +1,43 @@
 extends CharacterBody2D
 class_name Player
 
-const _tile_size: Vector2 = Vector2(16,16)
+const _tile_size: Vector2 = Vector2(32,32)
 var _sprite_node_pos_tween: Tween
 
 @onready var _up =  $up as RayCast2D
 @onready var _down =  $down as RayCast2D
 @onready var _left =  $left as RayCast2D
 @onready var _right =  $right as RayCast2D
+@onready var _sprite := $AnimatedSprite2D as AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
 	if !_sprite_node_pos_tween or !_sprite_node_pos_tween.is_running():
+		_sprite.stop()
 		if Input.is_action_pressed("ui_up") and !_up.is_colliding():
 			_move(Vector2(0, -1))
+			_sprite.play("walk_up")
 			return
 		if Input.is_action_pressed("ui_down") and !_down.is_colliding():
 			_move(Vector2(0, 1))
+			_sprite.play("walk_down")
 			return
 		if Input.is_action_pressed("ui_left") and !_left.is_colliding():
 			_move(Vector2(-1, 0))
+			_sprite.flip_h = true
+			_sprite.play("walk_right")
 			return
 		if Input.is_action_pressed("ui_right") and !_right.is_colliding():
 			_move(Vector2(1, 0))
+			_sprite.flip_h = false
+			_sprite.play("walk_right")
 			return
 
 func _move(dir: Vector2):
 	global_position += dir * _tile_size
-	$Sprite2D.global_position -= dir * _tile_size
+	_sprite.global_position -= dir * _tile_size
 
 	if _sprite_node_pos_tween:
 		_sprite_node_pos_tween.kill()
 	_sprite_node_pos_tween = create_tween()
 	_sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	_sprite_node_pos_tween.tween_property($Sprite2D, "global_position", global_position, 0.25)
+	_sprite_node_pos_tween.tween_property(_sprite, "global_position", global_position, 0.25)
