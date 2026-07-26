3034592642d2854fda7e000099b98d3f19d6a949
PandaFarmer <iqiao2011@gmail.com>
2026-07-25 17:34:30 -0700

    made intro level and added level transition via player group signal

--- Isaac/scripts/scene_change.gd
+++ Isaac/scripts/scene_change.gd
@@ -2,8 +2,9 @@ extends Area2D
 
 @export_file var scene_path: String
 
-
 func _on_body_entered(body):
+	print("collision")
 	if body.is_in_group("player"):
+		print("player collision")
 		#if get_node("../..").keys >= get_node("../..").required_keys:
 		get_tree().change_scene_to_file.call_deferred(scene_path)
